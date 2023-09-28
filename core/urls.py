ce9129b7e09b231126129554656d2483da8159ef
rodrigobrito23 <rodrigo_brito11@hotmail.com>
2023-09-27 23:35:26 -0300

    update

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import index, contato, produto
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('', index, name='index'),
     path('contato', contato, name='contato'),
     path('produto/<int:pk>', produto, name='produto'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
