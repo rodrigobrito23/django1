3196d833a2cb4e541b916562b8aaa7f3824b391b
rodrigobrito23 <rodrigo_brito11@hotmail.com>
2023-09-27 23:22:12 -0300

    update

--- django1/settings.py
+++ django1/settings.py
@@ -124,11 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/' #usado durante o desenvolvimento
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'), ]
+#STATICFILES_DIRS = [os.path.join(BASE_DIR,'static'), ]
 
 LOGOUT_REDIRECT_URL = 'index'
 
-#STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
